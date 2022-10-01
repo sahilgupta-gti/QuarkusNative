80c7d607c0844442d6ce4e7726f501eb13904849
Author Name <sahilgupta_gti@yahoo.com>
2022-10-01 12:15:54 +0530

    native image details updated

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ Goto nativeimage module directory
 
 ```shell
 mvn package -Pnative
+```
+
+# Run docker image
+```shell
+docker run --rm -p 8080:8080 --name quarkus <image-name>
+```
+
+# Execute http endpoint
+```bash
+curl -v -s http://$(minikube ip):8080/jackson/|jq -C
+```
--- nativeimage/src/main/java/org/example/beans/impl/SampleServiceImpl.java
+++ nativeimage/src/main/java/org/example/beans/impl/SampleServiceImpl.java
@@ -1,13 +1,25 @@
 package org.example.beans.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.beans.SampleService;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
 @ApplicationScoped
 public class SampleServiceImpl implements SampleService {
+    SampleServiceImpl() {
+        log.info("SampleServiceImpl constructor called: {}", this);
+    }
+
     @Override
-    public String getResponse() {
-        return "response from SampleService";
+    public Map<String, String> getResponse() {
+        Map<String, String> map = new HashMap<>();
+        map.put("response", this.getClass().getName());
+        map.put("timestamp", LocalDateTime.now().toString());
+        return map;
     }
 }
